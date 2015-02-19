f68b74e1be5d2db88baa74413e0b1af78d480370
David <david.recuencogadea@gmail.com>
2015-02-19 01:39:37 +0100

    Frontend image POST

--- src/main/webapp/js/controllers.js
+++ src/main/webapp/js/controllers.js
@@ -1,9 +1,25 @@
 angular.module('starter')
 
-    .controller('MainCtrl', [ '$scope', function($scope){
-        $scope.imageFile="";
+    .service('fileUpload', ['$http', function ($http) {
+        this.uploadFileToUrl = function(file, uploadUrl){
+            var fd = new FormData();
+            fd.append('file', file);
+            $http.post(uploadUrl, fd, {
+                transformRequest: angular.identity,
+                headers: {'Content-Type': undefined}
+            }).success(function(){
 
-        $scope.submitImage = function(){
-            alert($scope.imageFile);
+            }).error(function(){
+
+            });
         }
+    }])
+
+    .controller('MainCtrl', [ '$scope', 'fileUpload', function($scope, fileUpload){
+        $scope.uploadFile = function(){
+            var file = $scope.myFile;
+            console.log('file is ' + JSON.stringify(file));
+            var uploadUrl = "http://127.0.0.1:5000/api/upload";
+            fileUpload.uploadFileToUrl(file, uploadUrl);
+        };
     }]);
--- src/main/webapp/js/directives.js
+++ src/main/webapp/js/directives.js
@@ -5,4 +5,20 @@ angular.module('starter')
             restrict: 'E',
             templateUrl: 'templates/components/footer.html'
         }
-    });
+    })
+
+    .directive('fileModel', ['$parse', function($parse){
+        return {
+            restrict: 'A',
+            link: function(scope, element, attrs) {
+                var model = $parse(attrs.fileModel);
+                var modelSetter = model.assign;
+
+                element.bind('change', function(){
+                    scope.$apply(function(){
+                        modelSetter(scope, element[0].files[0]);
+                    })
+                })
+            }
+        }
+    }]);
